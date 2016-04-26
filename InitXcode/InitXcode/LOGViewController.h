e27d6a2c00bc8d80606e8d9dffa45b4f79e90449
cwl <1178686275@qq.com>
2016-04-26 11:15:22 +0800

    add LogVC

---
+++ InitXcode/InitXcode/LOGViewController.h
@@ -0,0 +1,13 @@
+//
+//  LOGViewController.h
+//  InitXcode
+//
+//  Created by chengweilin on 16/4/26.
+//  Copyright © 2016年 chengweilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOGViewController : UIViewController
+
+@end
