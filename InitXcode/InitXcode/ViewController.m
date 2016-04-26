a2224026ac1d1f2cc3c8850e1be1fb16e1aaed02
cwl <1178686275@qq.com>
2016-04-26 10:38:48 +0800

    init Xcode

---
+++ InitXcode/InitXcode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InitXcode
+//
+//  Created by chengweilin on 16/4/26.
+//  Copyright © 2016年 chengweilin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Git is a version control system.");
+    NSLog(@"Git is free software.");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
